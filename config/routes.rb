f72851bc3452b058fc0e8675d12a2f1a92491dd7
Vladimir Petrachuk <hippozz@yandex.ru>
2016-03-13 18:58:08 +0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
   resources :clients
 	root 'contracts#index'
 
+  match '/contracts', to: 'contracts#index', via: 'get'
+  match '/insurance_types', to: 'insurance_types#index', via: 'get'
+  match '/autos', to: 'autos#index', via: 'get'
+  match '/insurers', to: 'insurers#index', via: 'get'
+  match '/clients', to: 'clients#index', via: 'get'
+
 
 
 end
